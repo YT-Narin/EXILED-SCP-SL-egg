7e6ba4c1ea5589059d12a90bed3c3abb14cdb339
Jonathan Esser <36900138+EsserGaming@users.noreply.github.com>
2023-06-28 21:39:10 -0500

    Add cleanup
    Small addition to remove any unnecessary files, there are probably more I could add but for now this is enough.

--- script.sh
+++ script.sh
@@ -22,7 +22,7 @@ $(tput setaf 2)This installer was created by $(tput setaf 1)Parkeymon#0001$(tput
 "
 
 # Egg version checking, do not touch!
-currentVersion="2.5.0"
+currentVersion="2.5.1"
 latestVersion=$(curl --silent "https://api.github.com/repos/Parkeymon/EXILED-SCP-SL-egg/releases/latest" | jq -r .tag_name)
 
 if [ "${currentVersion}" == "${latestVersion}" ]; then
@@ -226,5 +226,12 @@ if [ "${INSTALL_CUSTOM}" == "true" ]; then
   done
 
 fi
+
+# Cleanup :p
+rm /mnt/server/core
+rm /mnt/server/Exiled.Installer-Linux
+rm -rf /mnt/server/?
+rm -rf /mnt/server/.local
+rm /mnt/server/config-gameplay.txt
 
 echo "$(tput setaf 2)Installation Complete!$(tput sgr 0)"
